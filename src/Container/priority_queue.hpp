0dc09d70ad40588b0669aff1509331ec6bf5071b
Nathan Phipps <naphipps@noreply.com>
2021-09-13 20:05:36 -0500

    added priority_queue hpp

---
+++ src/Container/priority_queue.hpp
@@ -0,0 +1,25 @@
+//
+//  priority_queue.hpp
+//  NP-Engine
+//
+//  Created by Nathan Phipps on 7/25/21.
+//
+
+#ifndef NP_ENGINE_PRIORITY_QUEUE_HPP
+#define NP_ENGINE_PRIORITY_QUEUE_HPP
+
+#include <queue>
+
+#include "vector.hpp"
+
+namespace np::container
+{
+    template <
+    class T,
+    class Container = vector<T>, //TODO: how do we determine allocator?! maybe we need to inherit from std priority queue?
+    class Compare = ::std::less<typename Container::value_type>
+    >
+    using pqueue = ::std::priority_queue<T, Container, Compare>;
+}
+
+#endif /* NP_ENGINE_PRIORITY_QUEUE_HPP */
