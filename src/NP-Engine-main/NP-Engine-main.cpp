94cb38780719657eb2cecfa89ff7d856fe607fe8
Nathan Phipps <naphipps@noreply.com>
2021-11-09 15:21:48 -0600

    updated NP-Engine main - better layout and exception handling

--- src/NP-Engine-main/NP-Engine-main.cpp
+++ src/NP-Engine-main/NP-Engine-main.cpp
@@ -12,57 +12,61 @@ extern ::np::app::Application* ::np::app::CreateApplication(::np::memory::Alloca
 
 i32 main(i32 argc, chr** argv)
 {
-    ::np::system::Init(); //TODO: register TraitAllocator here maybe??
-
     i32 retval = 0;
     str message;
     ::np::system::Popup::Style style = ::np::system::Popup::DefaultStyle;
     ::np::system::Popup::Buttons buttons = ::np::system::Popup::DefaultButtons;
 
-    ::np::memory::CAllocator& main_allocator = ::np::memory::DefaultAllocator;
-    ::np::memory::Block main_block = main_allocator.Allocate(NP_ENGINE_MAIN_MEMORY_SIZE);
-    
-    if (main_block.IsValid())
+    try
     {
-        try
+        ::np::system::Init();
+        ::np::memory::CAllocator& main_allocator = ::np::memory::DefaultAllocator;
+        ::np::memory::Block main_block = main_allocator.Allocate(NP_ENGINE_MAIN_MEMORY_SIZE);
+
+        if (main_block.IsValid())
         {
             ::np::memory::RedBlackTreeAllocator application_allocator(main_block);
-            ::np::app::Application* app = ::np::app::CreateApplication(application_allocator);
-            app->Run(argc, argv);
-            ::np::memory::Destruct(app);
-            application_allocator.Deallocate(app);
+            ::np::memory::TraitAllocator::Register(application_allocator);
+            ::np::app::Application* application = ::np::app::CreateApplication(application_allocator);
+            application->Run(argc, argv);
+            ::np::memory::Destruct(application);
+            application_allocator.Deallocate(application);
+            ::np::memory::TraitAllocator::Register(::np::memory::DefaultAllocator);
         }
-        catch (const ::std::exception& e)
+        else
         {
-            retval = 1;
-            message = "STD EXCEPTION OCCURRED: " + to_str(e.what()) + "\n";
+            retval = 3;
+            message = "WAS NOT ABLE TO ALLOCATE ENOUGH MEMORY\n";
             message += "Log file can be found here: " + ::np::insight::Log::GetFileLoggerFilePath();
             style = ::np::system::Popup::Style::Error;
-            NP_LOG_FILE_ERROR(message);
+            NP_LOG_ERROR(message);
         }
-        catch (...)
-        {
-            retval = 2;
-            message = "SOME OTHER EXCEPTION OCCURRED\n";
-            message += "Log file can be found here: " + ::np::insight::Log::GetFileLoggerFilePath();
-            style = ::np::system::Popup::Style::Error;
-            NP_LOG_FILE_ERROR(message);
-        }
+
+        main_allocator.Deallocate(main_block);
     }
-    else
+    catch (const ::std::exception& e)
     {
-        retval = 3;
-        message = "WAS NOT ABLE TO ALLOCATE ENOUGH MEMORY\n";
+        ::np::memory::TraitAllocator::Register(::np::memory::DefaultAllocator);
+        retval = 1;
+        message = "STD EXCEPTION OCCURRED: \n" + to_str(e.what()) + "\n\n";
         message += "Log file can be found here: " + ::np::insight::Log::GetFileLoggerFilePath();
         style = ::np::system::Popup::Style::Error;
-        NP_LOG_FILE_ERROR(message);
+        NP_LOG_ERROR(message);
     }
-
+    catch (...)
+    {
+        ::np::memory::TraitAllocator::Register(::np::memory::DefaultAllocator);
+        retval = 2;
+        message = "UNKNOWN EXCEPTION OCCURRED\n\n";
+        message += "Log file can be found here: " + ::np::insight::Log::GetFileLoggerFilePath();
+        style = ::np::system::Popup::Style::Error;
+        NP_LOG_ERROR(message);
+    }
+    
     if (retval != 0)
     {
         ::np::system::Popup::Show("NP-Engine Code: " + to_str(retval), message, style, buttons);
     }
     
-    main_allocator.Deallocate(main_block);
     return retval;
 }
