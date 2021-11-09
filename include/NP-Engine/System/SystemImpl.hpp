744063f6c1a5f6b27c3e1e00b8856458d14336cb
Nathan Phipps <naphipps@noreply.com>
2021-11-09 14:58:02 -0600

    added terminate and signal handlers - set via system::Init

--- include/NP-Engine/System/SystemImpl.hpp
+++ include/NP-Engine/System/SystemImpl.hpp
@@ -4,15 +4,69 @@
 #define NP_ENGINE_SYSTEM_IMPL_HPP
 
 #include <cstdlib>
+#include <iostream>
+#include <csignal>
+#include <exception>
 
 #include "NP-Engine/Filesystem/Filesystem.hpp"
 #include "NP-Engine/Platform/Platform.hpp"
 #include "NP-Engine/Primitive/Primitive.hpp"
+#include "NP-Engine/Insight/Insight.hpp"
+
+#include "Popup.hpp"
 
 //TODO: add summary comments
 
 namespace np::system
 {
+    namespace __detail
+    {
+        static inline void terminate_handler() noexcept
+        {
+            ::std::cerr << "The terminate function was called.\nLog file can be found here : " + insight::Log::GetFileLoggerFilePath() << "\n";
+            Popup::Show("NP-Engine Terminate Function Called",
+                "Probably an unhandled exception was thrown.\nLog file can be found here : " + insight::Log::GetFileLoggerFilePath(),
+                Popup::Style::Error, Popup::Buttons::OK);
+        }
+
+        static inline void signal_handler(int signal) noexcept
+        {
+            str signal_string = "";
+            switch (signal)
+            {
+            case SIGINT:
+                signal_string = "SIGINT";
+                break;
+            case SIGILL:
+                signal_string = "SIGILL";
+                break;
+            case SIGFPE:
+                signal_string = "SIGFPE";
+                break;
+            case SIGSEGV:
+                signal_string = "SIGSEGV";
+                break;
+            case SIGTERM:
+                signal_string = "SIGTERM";
+                break;
+            case SIGBREAK:
+                signal_string = "SIGBREAK";
+                break;
+            case SIGABRT:
+                signal_string = "SIGABRT";
+                break;
+            default:
+                signal_string = "UNKNOWN SIGNAL";
+                break;
+            }
+
+            ::std::cerr << signal_string + " was raised.\nLog file can be found here : " + insight::Log::GetFileLoggerFilePath() << "\n";
+            Popup::Show("NP-Engine Signal Raised",
+                signal_string + " was raised.\nLog file can be found here : " + insight::Log::GetFileLoggerFilePath(),
+                Popup::Style::Error, Popup::Buttons::OK);
+        }
+    }
+
 	/**
 	 gets the value of the given env variable
 	*/
@@ -41,6 +95,14 @@ namespace np::system
 		str working_dir = GetDefaultWorkingDir();
 		fs::create_directories(working_dir);
 		fs::set_current_path(working_dir);
+
+        ::std::set_terminate(__detail::terminate_handler);
+        ::std::signal(SIGABRT, __detail::signal_handler);
+        ::std::signal(SIGFPE, __detail::signal_handler);
+        ::std::signal(SIGILL, __detail::signal_handler);
+        ::std::signal(SIGINT, __detail::signal_handler);
+        ::std::signal(SIGSEGV, __detail::signal_handler);
+        ::std::signal(SIGTERM, __detail::signal_handler);
 	}
 }
 
