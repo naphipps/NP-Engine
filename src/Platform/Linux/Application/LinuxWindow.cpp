051bab87a61d7757f81eb4aef0e5e8f348101ad3
Nathan Phipps <naphipps@noreply.com>
2021-11-18 09:07:35 -0600

    updated LinuxWindow

--- src/Platform/Linux/Application/LinuxWindow.cpp
+++ src/Platform/Linux/Application/LinuxWindow.cpp
@@ -5,10 +5,4 @@
 //  Created by Nathan Phipps on 11/2/21.
 //
 
-#include "NP-Engine/Platform/Linux/Application/LinuxWindow.hpp"
-
-namespace np::app
-{
-	ui32 LinuxWindow::_window_count = 0;
-	Mutex LinuxWindow::_mutex;
-}
+//#include "NP-Engine/Platform/Linux/Application/LinuxWindow.hpp"
