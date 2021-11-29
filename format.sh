346cb81183ff66e905ba983145906dfcc0fd396c
Nathan Phipps <naphipps@noreply.com>
2021-11-29 07:05:51 -0600

    updated format.sh

--- format.sh
+++ format.sh
@@ -1,3 +1,18 @@
 #! /bin/sh
 
-find . -type f -name "*.[ch]pp" \( -path "./test/*" -o -path "./include/*" -o -path "./src/*" \) -prune | xargs clang-format -i
+format="clang-format"
+
+if ! command -v ${format} >/dev/null 2>&1
+then
+	format="clang-format-13"
+fi
+
+if ! command -v ${format} >/dev/null 2>&1
+then
+	echo "clang-format could not be found"
+	exit
+fi
+
+echo "Using clang-format: $(${format} --version)"
+
+find . -type f -name "*.[ch]pp" \( -path "./test/*" -o -path "./include/*" -o -path "./src/*" \) -prune | xargs ${format} -i
