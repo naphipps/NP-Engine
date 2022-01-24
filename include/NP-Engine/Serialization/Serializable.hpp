fddd6de1d47378b150bff57e5fdb23d3e572a126
Nathan Phipps <naphipps@noreply.com>
2022-01-24 10:53:41 -0600

    fixed cleanup in Serializable

--- include/NP-Engine/Serialization/Serializable.hpp
+++ include/NP-Engine/Serialization/Serializable.hpp
@@ -17,158 +17,155 @@
 
 #include "NP-Engine/Vendor/JsonInclude.hpp"
 
-namespace np
+namespace np::serialization
 {
-	namespace serialization
+	/**
+	 this class acts as the basis for all serializable objects
+	 the entry points still enable inherited behavior while the protected methods allow for serialization coordination
+	 throughout inheritance
+	 */
+	class Serializable
 	{
+	protected:
 		/**
-		 this class acts as the basis for all serializable objects
-		 the entry points still enable inherited behavior while the protected methods allow for serialization coordination
-		 throughout inheritance
+		 protected template Save method so child classes can directly call the Save of their parents
 		 */
-		class Serializable
+		template <typename T>
+		bl SaveAs(str filepath, const T* object) const
 		{
-		protected:
-			/**
-			 protected template Save method so child classes can directly call the Save of their parents
-			 */
-			template <typename T>
-			bl SaveAs(str filepath, const T* object) const
+			bl saved = false;
+
+			str parent_path = fs::GetParentPath(filepath);
+
+			if (parent_path.size() > 0)
 			{
-				bl saved = false;
-
-				str parent_path = fs::GetParentPath(filepath);
-
-				if (parent_path.size() > 0)
-				{
-					fs::CreateDirectories(parent_path);
-				}
-
-				if ((parent_path.size() == 0 && filepath.size() > 0) || fs::Exists(parent_path))
-				{
-					ofstrm os;
-					os.open(filepath);
-					object->T::Insertion(os, filepath);
-					os.close();
-
-					saved = true;
-				}
-
-				return saved;
+				fs::CreateDirectories(parent_path);
 			}
 
-			/**
-			protected template Load method so child classes can directly call the Load of their parents
-			*/
-			template <typename T>
-			bl LoadAs(str filepath, T* object)
+			if ((parent_path.size() == 0 && filepath.size() > 0) || fs::Exists(parent_path))
 			{
-				bl loaded = false;
+				::std::ofstream os;
+				os.open(filepath);
+				object->T::Insertion(os, filepath);
+				os.close();
 
-				if (fs::Exists(filepath))
-				{
-					ifstrm is;
-					is.open(filepath);
-					object->T::Extraction(is, filepath);
-					is.close();
-
-					loaded = true;
-				}
-
-				return loaded;
+				saved = true;
 			}
 
-		public:
-			/**
-			 save ourselves to the given json object
-			 */
-			virtual bl SaveToJson(nlohmann::json& json) const
+			return saved;
+		}
+
+		/**
+		protected template Load method so child classes can directly call the Load of their parents
+		*/
+		template <typename T>
+		bl LoadAs(str filepath, T* object)
+		{
+			bl loaded = false;
+
+			if (fs::Exists(filepath))
 			{
-				return false;
+				::std::ifstream is;
+				is.open(filepath);
+				object->T::Extraction(is, filepath);
+				is.close();
+
+				loaded = true;
 			}
 
-			/**
-			 load ourselves from the given json object
-			 */
-			virtual bl LoadFromJson(const nlohmann::json& json)
+			return loaded;
+		}
+
+	public:
+		/**
+		 save ourselves to the given json object
+		 */
+		virtual bl SaveToJson(nlohmann::json& json) const
+		{
+			return false;
+		}
+
+		/**
+		 load ourselves from the given json object
+		 */
+		virtual bl LoadFromJson(const nlohmann::json& json)
+		{
+			return false;
+		}
+
+		/**
+		 save oursellves as the given filepath
+		 return if the save was successful or not
+		 */
+		bl SaveAs(str filepath) const
+		{
+			bl saved = false;
+
+			str parent_path = fs::GetParentPath(filepath);
+
+			if (parent_path.size() > 0)
 			{
-				return false;
+				fs::CreateDirectories(parent_path);
 			}
 
-			/**
-			 save oursellves as the given filepath
-			 return if the save was successful or not
-			 */
-			bl SaveAs(str filepath) const
+			if ((parent_path.size() == 0 && filepath.size() > 0) || fs::Exists(parent_path))
 			{
-				bl saved = false;
+				::std::ofstream os;
+				os.open(filepath);
+				Insertion(os, filepath);
+				os.close();
 
-				str parent_path = fs::GetParentPath(filepath);
-
-				if (parent_path.size() > 0)
-				{
-					fs::CreateDirectories(parent_path);
-				}
-
-				if ((parent_path.size() == 0 && filepath.size() > 0) || fs::Exists(parent_path))
-				{
-					::std::ofstream os;
-					os.open(filepath);
-					Insertion(os, filepath);
-					os.close();
-
-					saved = true;
-				}
-
-				return saved;
+				saved = true;
 			}
 
-			/**
-			 load outselves as the given filepath
-			 return if the load was successful or not
-			 */
-			bl LoadAs(str filepath)
+			return saved;
+		}
+
+		/**
+		 load outselves as the given filepath
+		 return if the load was successful or not
+		 */
+		bl LoadAs(str filepath)
+		{
+			bl loaded = false;
+
+			if (fs::Exists(filepath))
 			{
-				bl loaded = false;
+				::std::ifstream is;
+				is.open(filepath);
+				Extraction(is, filepath);
+				is.close();
 
-				if (fs::Exists(filepath))
-				{
-					::std::ifstream is;
-					is.open(filepath);
-					Extraction(is, filepath);
-					is.close();
-
-					loaded = true;
-				}
-
-				return loaded;
+				loaded = true;
 			}
 
-			/**
-			 serialization method for us to write
-			 we require our objects to know which filepath they are a part of
-			 */
-			virtual ::std::ostream& Insertion(::std::ostream& os, str filepath) const = 0;
+			return loaded;
+		}
 
-			/**
-			 deserialization method for us to read
-			 we require our objects to know which filepath they are a part of
-			 */
-			virtual ::std::istream& Extraction(::std::istream& is, str filepath) = 0;
+		/**
+		 serialization method for us to write
+		 we require our objects to know which filepath they are a part of
+		 */
+		virtual ::std::ostream& Insertion(::std::ostream& os, str filepath) const = 0;
 
-			/**
-			 save oursellves inside the given dirpath
-			 return if the save was successful or not
-			 */
-			virtual bl SaveTo(str dirpath) const = 0;
+		/**
+		 deserialization method for us to read
+		 we require our objects to know which filepath they are a part of
+		 */
+		virtual ::std::istream& Extraction(::std::istream& is, str filepath) = 0;
 
-			/**
-			 load outselves from the given dirpath
-			 return if the load was successful or not
-			 */
-			virtual bl LoadFrom(str dirpath) = 0;
-		};
-	} // namespace serialization
-} // namespace np
+		/**
+		 save oursellves inside the given dirpath
+		 return if the save was successful or not
+		 */
+		virtual bl SaveTo(str dirpath) const = 0;
+
+		/**
+		 load outselves from the given dirpath
+		 return if the load was successful or not
+		 */
+		virtual bl LoadFrom(str dirpath) = 0;
+	};
+} // namespace np::serialization
 
 #endif /* NP_ENGINE_SERIALIZABLE_HPP */
