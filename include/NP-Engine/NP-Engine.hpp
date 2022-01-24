42daf42021ffc1be14c5c1da0e49df692a29ce30
Nathan Phipps <naphipps@noreply.com>
2022-01-24 09:28:54 -0600

    cleanup in NP-Engine.hpp and NP-Engine-main.cpp

--- include/NP-Engine/NP-Engine.hpp
+++ include/NP-Engine/NP-Engine.hpp
@@ -1,37 +1,35 @@
+//##===----------------------------------------------------------------------===##//
 //
-//  NP-Engine.hpp
-//  NP-Engine
+//  Author: Nathan Phipps 2/10/21
 //
-//  Created by Nathan Phipps on 2/10/21.
-//
+//##===----------------------------------------------------------------------===##//
 
 #ifndef NP_ENGINE_HPP
 #define NP_ENGINE_HPP
 
-// TODO: cleanup includes
-
 #include "Foundation/Foundation.hpp"
-#include "Serialization/Serialization.hpp"
-#include "Math/Math.hpp"
-#include "Random/Random.hpp"
-#include "Filesystem/Filesystem.hpp"
-#include "Concurrency/Concurrency.hpp"
-#include "Insight/Insight.hpp"
-#include "Container/Container.hpp"
-#include "Primitive/Primitive.hpp"
-#include "Core/Core.hpp"
-#include "JobSystem/JobSystem.hpp"
-#include "Noise/Noise.hpp"
-#include "Serialization/Serialization.hpp"
-#include "System/System.hpp"
-#include "Physics/Physics.hpp"
-#include "Time/Time.hpp"
+
 #include "Application/Application.hpp"
-#include "Graphics/Graphics.hpp"
-#include "Event/Event.hpp"
-#include "Platform/Platform.hpp"
-#include "Input/Input.hpp"
+#include "Concurrency/Concurrency.hpp"
+#include "Container/Container.hpp"
+#include "Core/Core.hpp"
 #include "ECS/ECS.hpp"
+#include "Event/Event.hpp"
+#include "Filesystem/Filesystem.hpp"
+#include "Graphics/Graphics.hpp"
+#include "Input/Input.hpp"
+#include "Insight/Insight.hpp"
+#include "JobSystem/JobSystem.hpp"
+#include "Math/Math.hpp"
+#include "Noise/Noise.hpp"
+#include "Physics/Physics.hpp"
+#include "Platform/Platform.hpp"
+#include "Primitive/Primitive.hpp"
+#include "Random/Random.hpp"
+#include "Serialization/Serialization.hpp"
+#include "Serialization/Serialization.hpp"
 #include "String/String.hpp"
+#include "System/System.hpp"
+#include "Time/Time.hpp"
 
 #endif /* NP_ENGINE_HPP */
