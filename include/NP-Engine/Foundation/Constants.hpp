9d92f8e3392f71fbd0c7a56d847452818df4dc0e
Nathan Phipps <naphipps@noreply.com>
2022-02-14 14:14:56 -0600

    Foundation now includes Platform instead of other way around - updated comments

--- include/NP-Engine/Foundation/Constants.hpp
+++ include/NP-Engine/Foundation/Constants.hpp
@@ -1,10 +1,8 @@
+//##===----------------------------------------------------------------------===##//
 //
-//  Constants.hpp
-//  Project Space
+//  Author: Nathan Phipps 5/20/20
 //
-//  Created by Nathan Phipps on 5/20/20.
-//  Copyright © 2020 Nathan Phipps. All rights reserved.
-//
+//##===----------------------------------------------------------------------===##//
 
 #ifndef NP_ENGINE_CONSTANTS_HPP
 #define NP_ENGINE_CONSTANTS_HPP
--- include/NP-Engine/Foundation/ConstantsSettings.hpp
+++ include/NP-Engine/Foundation/ConstantsSettings.hpp
@@ -7,6 +7,8 @@
 #ifndef NP_ENGINE_CONSTANTS_SETTINGS_HPP
 #define NP_ENGINE_CONSTANTS_SETTINGS_HPP
 
+#include "NP-Engine/Platform/Platform.hpp"
+
 #include "cmake_include.hpp"
 
 #ifndef NP_ENGINE_MAIN_MEMORY_SIZE
@@ -22,5 +24,12 @@
 // The following durations are milliseconds
 #define NP_ENGINE_APPLICATION_LOOP_DURATION 4
 #define NP_ENGINE_WINDOW_LOOP_DURATION 8
+
+// github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+#if NP_ENGINE_PLATFORM_IS_WINDOWS
+	#ifndef NOMINMAX
+		#define NOMINMAX
+	#endif
+#endif
 
 #endif /* NP_ENGINE_CONSTANTS_SETTINGS_HPP */
