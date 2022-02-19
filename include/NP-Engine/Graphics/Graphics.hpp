f93b0e598628b4e34a6a75739821c6fff30122f9
Nathan Phipps <naphipps@noreply.com>
2022-02-18 18:25:06 -0600

    cleanup in Graphics.hpp

--- include/NP-Engine/Graphics/Graphics.hpp
+++ include/NP-Engine/Graphics/Graphics.hpp
@@ -9,25 +9,6 @@
 
 #include "NP-Engine/Foundation/Foundation.hpp"
 
-#include "NP-Engine/Primitive/Primitive.hpp"
-#include "NP-Engine/Platform/Platform.hpp"
-#include "NP-Engine/Container/Container.hpp"
-#include "NP-Engine/Memory/Memory.hpp"
-#include "NP-Engine/Insight/Insight.hpp"
-#include "NP-Engine/System/System.hpp"
-#include "NP-Engine/String/String.hpp"
-
-#include "NP-Engine/Vendor/VulkanInclude.hpp"
-#include "NP-Engine/Vendor/GlmInclude.hpp"
-
 #include "RPI/RPI.hpp"
-
-namespace np::graphics
-{
-	// TODO: figure out if we need these functions, and if so, should be put them in Graphics.cpp?
-	static void Init() {}
-
-	static void Shutdown() {}
-} // namespace np::graphics
 
 #endif /* NP_ENGINE_GRAPHICS_HPP */
