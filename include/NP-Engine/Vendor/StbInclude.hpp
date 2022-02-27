bd8f453995fe806b69c60f7f2d79177b37bcfb41
Nathan Phipps <naphipps@noreply.com>
2022-02-26 22:36:47 -0600

    added vendor stb

---
+++ include/NP-Engine/Vendor/StbInclude.hpp
@@ -0,0 +1,13 @@
+//##===----------------------------------------------------------------------===##//
+//
+//  Author: Nathan Phipps 12/18/21
+//
+//##===----------------------------------------------------------------------===##//
+
+#ifndef NP_ENGINE_VENDOR_STB_INCLUDE_HPP
+#define NP_ENGINE_VENDOR_STB_INCLUDE_HPP
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#endif /* NP_ENGINE_VENDOR_STB_INCLUDE_HPP */
