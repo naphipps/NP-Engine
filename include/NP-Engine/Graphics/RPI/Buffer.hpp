fbffdff3faaaec48d583708e20ba7815032b62ba
Nathan Phipps <naphipps@noreply.com>
2022-03-01 12:38:02 -0600

    added todo comment

--- include/NP-Engine/Graphics/RPI/Buffer.hpp
+++ include/NP-Engine/Graphics/RPI/Buffer.hpp
@@ -10,7 +10,9 @@
 namespace np::graphics
 {
 	class Buffer
-	{};
+	{
+		// TODO: should we add a create function taking templated args to pass along to rhi constructors?
+	};
 } // namespace np::graphics
 
 #endif /* NP_ENGINE_RPI_BUFFER_HPP */
