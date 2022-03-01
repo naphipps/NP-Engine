aadd5517fcc81435218aca1f970793040848eb6e
Nathan Phipps <naphipps@noreply.com>
2022-03-01 12:26:58 -0600

    added header defines in Buffer.hpp

--- include/NP-Engine/Graphics/RPI/Buffer.hpp
+++ include/NP-Engine/Graphics/RPI/Buffer.hpp
@@ -4,8 +4,13 @@
 //
 //##===----------------------------------------------------------------------===##//
 
+#ifndef NP_ENGINE_RPI_BUFFER_HPP
+#define NP_ENGINE_RPI_BUFFER_HPP
+
 namespace np::graphics
 {
 	class Buffer
 	{};
 } // namespace np::graphics
+
+#endif /* NP_ENGINE_RPI_BUFFER_HPP */
