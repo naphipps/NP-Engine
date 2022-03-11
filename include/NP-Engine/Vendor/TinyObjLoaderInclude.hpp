8fc24c545d24be4366d1267ea21c7a003284eb3a
Nathan Phipps <naphipps@noreply.com>
2022-03-11 10:42:17 -0600

    added tinyobjloader - needs support object before actual use - wip

---
+++ include/NP-Engine/Vendor/TinyObjLoaderInclude.hpp
@@ -0,0 +1,14 @@
+//##===----------------------------------------------------------------------===##//
+//
+//  Author: Nathan Phipps 3/11/22
+//
+//##===----------------------------------------------------------------------===##//
+
+#ifndef NP_ENGINE_VENDOR_TINYOBJLOADER_INCLUDE_HPP
+#define NP_ENGINE_VENDOR_TINYOBJLOADER_INCLUDE_HPP
+
+// TODO: add Model and other support for tinyobjloader in RPI, etc
+
+#include <tiny_obj_loader.h>
+
+#endif /* NP_ENGINE_VENDOR_TINYOBJLOADER_INCLUDE_HPP */
