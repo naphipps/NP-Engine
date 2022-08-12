863537a0c6a6b1604ad98305c5ec48ae97028f5b
Nathan Phipps <naphipps@noreply.com>
2022-08-12 15:16:27 -0500

    updated log pattern

--- include/NP-Engine/Insight/Log.hpp
+++ include/NP-Engine/Insight/Log.hpp
@@ -34,7 +34,7 @@ namespace np::insight
 			bl expected = false;
 			if (_initialized.compare_exchange_strong(expected, true, mo_release, mo_relaxed))
 			{
-				::std::string pattern = "%^[pid:%P, tid:%t, %Y-%m-%d %H:%M:%S.%e] [%n, %l]%$\n\t%v\n";
+				::std::string pattern = "%^[%l, %n, %Y-%m-%d %H:%M:%S.%e, pid:%P, tid:%t]%$\n\t%v\n";
 
 				_stdout_sink = ::std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 				_stdout_sink->set_pattern(pattern);
