b22cfde41c66c3e6595559e68b0eb4922d3166a3
Nathan Phipps <naphipps@noreply.com>
2022-08-23 14:27:38 -0500

    cleaned up pnpoly

--- include/NP-Engine/Algorithms/pnpoly.hpp
+++ include/NP-Engine/Algorithms/pnpoly.hpp
@@ -13,145 +13,41 @@
 
 namespace np::alg
 {
-	// TODO: handle precalc_pnpoly from <http://alienryderflex.com/polygon/>
-	namespace precalc_pnpoly // TODO: refactor to __detail
-	{
-
-		//  Globals which should be set before calling these functions:
-		//
-		//  i32    polyCorners  =  how many corners the polygon has (no repeats)
-		//  flt  polyX[]      =  horizontal coordinates of corners
-		//  flt  polyY[]      =  vertical coordinates of corners
-		//  flt  x, y         =  point to be tested
-		//
-		//  The following global arrays should be allocated before calling these functions:
-		//
-		//  flt  constant[] = storage for precalculated constants (same size as polyX)
-		//  flt  multiple[] = storage for precalculated multipliers (same size as polyX)
-		//
-		//  (Globals are used in this example for purposes of speed.  Change as
-		//  desired.)
-		//
-		//  USAGE:
-		//  Call precalc_values() to initialize the constant[] and multiple[] arrays,
-		//  then call pointInPolygon(x, y) to determine if the point is in the polygon.
-		//
-		//  The function will return YES if the point x,y is inside the polygon, or
-		//  NO if it is not.  If the point is exactly on the edge of the polygon,
-		//  then the function may return YES or NO.
-		//
-		//  Note that division by zero is avoided because the division is protected
-		//  by the "if" clause which surrounds it.
-
-		/*
-		void precalc_values()
-		{
-			i32 i, j=polyCorners-1 ;
-
-			for(i=0; i<polyCorners; i++)
-			{
-				if(polyY[j]==polyY[i])
-				{
-					constant[i]=polyX[i];
-					multiple[i]=0;
-				}
-				else
-				{
-					constant[i]=polyX[i]-(polyY[i]*polyX[j])/(polyY[j]-polyY[i])+(polyY[i]*polyX[i])/(polyY[j]-polyY[i]);
-					multiple[i]=(polyX[j]-polyX[i])/(polyY[j]-polyY[i]);
-
-				}
-				j=i;
-
-			}
-
-		}
-
-		bl pointInPolygon_precal()
-		{
-			i32   i, j=polyCorners-1 ;
-			bl  oddNodes=NO      ;
-
-			for (i=0; i<polyCorners; i++)
-			{
-				if ((polyY[i]< y && polyY[j]>=y ||
-						polyY[j]< y && polyY[i]>=y))
-				{
-					oddNodes^=(y*multiple[i]+constant[i]<x);
-
-				}
-				j=i;
-			}
-
-			return oddNodes;
-		}
-
-		//smart optimization provided by Evgueni Tcherniaev
-		bl pointInPolygon_precal_optimized()
-		{
-
-			bl oddNodes=NO, current=polY[polyCorners-1]>y, previous;
-			for (i32 i=0; i<polyCorners; i++)
-			{
-				previous=current; current=polyY[i]>y;
-				if (current!=previous)
-				{
-					oddNodes^=y*multiple[i]+constant[i]<x;
-				}
-
-			}
-			return oddNodes;
-
-		}
-		//*/
-
-	} // namespace precalc_pnpoly
-
 	/*
-		//  Randolph Franklin pnpoly
-		//  Globals which should be set before calling this function:
-		//
-		//  i32    polyCorners  =  how many corners the polygon has (no repeats)
-		//  flt  polyX[]      =  horizontal coordinates of corners
-		//  flt  polyY[]      =  vertical coordinates of corners
-		//  flt  x, y         =  point to be tested
-		//
-		//  (Globals are used in this example for purposes of speed.  Change as
-		//  desired.)
-		//
-		//  The function will return YES if the point x,y is inside the polygon, or
-		//  NO if it is not.  If the point is exactly on the edge of the polygon,
-		//  then the function may return YES or NO.
-		//
-		//  Note that division by zero is avoided because the division is protected
-		//  by the "if" clause which surrounds it.
+		Randolph Franklin pnpoly <http://alienryderflex.com/polygon/>
+
+		poly = corners of the polygon (no repeats)
+		point = point to be tested
+
+		return true if point is inside poly, else false
+		if point is on the edge of the poly, then may return true or false
+
+		Note: division by zero is avoided because the division is protected
+		by the "if" clause which surrounds it
+
+		pnpoly optimizations provided by Nathan Mercer and Lascha Lagidse
+		precalc pnpoly optimization provided by Evgueni Tcherniaev
 	*/
+
 	template <typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
 	static bl pnpoly(const ::glm::vec<2, T, Q> poly[], const siz poly_size, const ::glm::vec<2, T, Q>& point)
 	{
 		dbl x = (dbl)point.x;
 		dbl y = (dbl)point.y;
-
 		siz i = 0;
-		siz j = poly_size - 1; // handle wrap around
+		siz j = poly_size - 1;
 		bl oddNodes = false;
-
 		for (i = 0; i < poly_size; i++)
 		{
-			if ((poly[i].y < y && poly[j].y >= y) || (poly[j].y < y && poly[i].y >= y))
+			if ((poly[i].y < y && poly[j].y >= y) || (poly[j].y < y && poly[i].y >= y) && (poly[i].x <= x && poly[j].x <= x))
 			{
-				if ((dbl)poly[i].x +
-						(dbl)(y - (dbl)poly[i].y) / (dbl)((dbl)poly[j].y - (dbl)poly[i].y) *
-							(dbl)((dbl)poly[j].x - (dbl)poly[i].x) <
-					x)
-				{
-					oddNodes = !oddNodes;
-				}
+				oddNodes ^= ((dbl)poly[i].x +
+								 (y - (dbl)poly[i].y) / ((dbl)poly[j].y - (dbl)poly[i].y) * ((dbl)poly[j].x - (dbl)poly[i].x) <
+							 x);
 			}
 
 			j = i;
 		}
-
 		return oddNodes;
 	}
 
@@ -159,6 +55,125 @@ namespace np::alg
 	static bl pnpoly(const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& poly, const ::glm::vec<2, T, Q>& point)
 	{
 		return pnpoly(poly.data(), poly.size(), point);
+	}
+
+	template <typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	void pnpoly_precalc(const ::glm::vec<2, T, Q> poly[], const siz poly_size, T constants[], T multiples[])
+	{
+		siz i;
+		siz j = poly_size - 1;
+		for (i = 0; i < poly_size; i++)
+		{
+			if (poly[j].y == poly[i].y)
+			{
+				constants[i] = poly[i].x;
+				multiples[i] = 0;
+			}
+			else
+			{
+				constants[i] = poly[i].x - (poly[i].y * poly[j].x) / (poly[j].y - poly[i].y) +
+					(poly[i].y * poly[i].x) / (poly[j].y - poly[i].y);
+				multiples[i] = (poly[j].x - poly[i].x) / (poly[j].y - poly[i].y);
+			}
+			j = i;
+		}
+	}
+
+	template <siz POLY_SIZE, typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	void pnpoly_precalc(const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& poly, T constants[], T multiples[])
+	{
+		pnpoly_precalc<T, Q>(poly.data(), poly.size(), constants, multiples);
+	}
+
+	template <typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	bl pnpoly(const ::glm::vec<2, T, Q> poly[], const siz poly_size, const T constants[], const T multiples[],
+			  const ::glm::vec<2, T, Q>& point)
+	{
+		bl oddNodes = false;
+		bl current = poly[poly_size - 1].y > point.y;
+		bl previous;
+		for (siz i = 0; i < poly_size; i++)
+		{
+			previous = current;
+			current = poly[i].y > point.y;
+			if (current != previous)
+				oddNodes ^= point.y * multiples[i] + constants[i] < point.x;
+		}
+		return oddNodes;
+	}
+
+	template <siz POLY_SIZE, typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	bl pnpoly(const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& poly, const T constants[], const T multiples[],
+			  const ::glm::vec<2, T, Q>& point)
+	{
+		return pnpoly<T, Q>(poly.data(), poly.size(), constants, multiples, point);
+	}
+
+	template <siz POLY_SIZE, typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	void pnpoly_precalc(const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& poly, con::array<T, POLY_SIZE>& constants,
+						con::array<T, POLY_SIZE>& multiples)
+	{
+		pnpoly_precalc<T, Q>(poly.data(), poly.size(), constants.data(), multiples.data());
+	}
+
+	template <siz POLY_SIZE, typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	bl pnpoly(const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& poly, const con::array<T, POLY_SIZE>& constants,
+			  const con::array<T, POLY_SIZE>& multiples, const ::glm::vec<2, T, Q>& point)
+	{
+		return pnpoly<T, Q>(poly.data(), poly.size(), constants.data(), multiples.data(), point);
+	}
+
+	template <typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	void pnpoly_precalc(const ::glm::vec<2, T, Q> poly[], const siz poly_size, ::glm::vec<2, T, Q> precalc[])
+	{
+		siz i;
+		siz j = poly_size - 1;
+		for (i = 0; i < poly_size; i++)
+		{
+			if (poly[j].y == poly[i].y)
+			{
+				precalc[i].x = poly[i].x;
+				precalc[i].y = 0;
+			}
+			else
+			{
+				precalc[i].x = poly[i].x - (poly[i].y * poly[j].x) / (poly[j].y - poly[i].y) +
+					(poly[i].y * poly[i].x) / (poly[j].y - poly[i].y);
+				precalc[i].y = (poly[j].x - poly[i].x) / (poly[j].y - poly[i].y);
+			}
+			j = i;
+		}
+	}
+
+	template <siz POLY_SIZE, typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	void pnpoly_precalc(const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& poly,
+						con::array<::glm::vec<2, T, Q>, POLY_SIZE>& precalc)
+	{
+		pnpoly_precalc<T, Q>(poly.data(), poly.size(), precalc.data());
+	}
+
+	template <typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	bl pnpoly(const ::glm::vec<2, T, Q> poly[], const siz poly_size, const ::glm::vec<2, T, Q> precalc[],
+			  const ::glm::vec<2, T, Q>& point)
+	{
+		bl oddNodes = false;
+		bl current = poly[poly_size - 1].y > point.y;
+		bl previous;
+		for (siz i = 0; i < poly_size; i++)
+		{
+			previous = current;
+			current = poly[i].y > point.y;
+			if (current != previous)
+				oddNodes ^= point.y * precalc[i].y + precalc[i].x < point.x;
+		}
+		return oddNodes;
+	}
+
+	template <siz POLY_SIZE, typename T, ::glm::qualifier Q = ::glm::qualifier::defaultp>
+	bl pnpoly(const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& poly, const con::array<::glm::vec<2, T, Q>, POLY_SIZE>& precalc,
+			  const ::glm::vec<2, T, Q>& point)
+	{
+		return pnpoly<T, Q>(poly.data(), poly.size(), precalc.data(), point);
 	}
 } // namespace np::alg
 
