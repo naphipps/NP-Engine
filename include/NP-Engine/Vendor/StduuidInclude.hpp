30f43b03f1fae8b9fd2adb77889add2a07ba1bac
Nathan Phipps <naphipps@noreply.com>
2022-08-30 14:01:30 -0500

    added vendor stduuid

---
+++ include/NP-Engine/Vendor/StduuidInclude.hpp
@@ -0,0 +1,12 @@
+//##===----------------------------------------------------------------------===##//
+//
+//  Author: Nathan Phipps 8/30/22
+//
+//##===----------------------------------------------------------------------===##//
+
+#ifndef NP_ENGINE_VENDOR_STDUUID_HPP
+#define NP_ENGINE_VENDOR_STDUUID_HPP
+
+#include "uuid.h"
+
+#endif /* NP_ENGINE_VENDOR_STDUUID_HPP */
