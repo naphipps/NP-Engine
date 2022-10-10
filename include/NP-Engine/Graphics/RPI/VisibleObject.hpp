f3b85888a0041ee5843942b49989e8d51e24cd7f
Nathan Phipps <naphipps@noreply.com>
2022-10-09 22:36:57 -0500

    added some more implementation to VisibleObject - wip

--- include/NP-Engine/Graphics/RPI/VisibleObject.hpp
+++ include/NP-Engine/Graphics/RPI/VisibleObject.hpp
@@ -9,11 +9,55 @@
 
 #include "NP-Engine/Geometry/Geometry.hpp"
 
+#include "RenderableLight.hpp"
+#include "RenderableModel.hpp"
+#include "RenderableImage.hpp"
+#include "RenderableType.hpp"
+
 namespace np::gfx
 {
-	struct VisibleObject
+	class VisibleObject
 	{
-		geom::DblSphere BoundingSphere;
+	private:
+		RenderableType _renderable_type = RenderableType::None;
+		void* _payload = nullptr;
+		
+	public:
+		geom::FltSphere BoundingSphere{}; //TODO: i think bounding AABB are going to be the way we go
+		
+		void* GetPayload() const
+		{
+			return _payload;
+		}
+		
+		void SetPayload(RenderableModel* renderable_model)
+		{
+			_renderable_type = RenderableType::Model;
+			_payload = renderable_model;
+		}
+		
+		void SetPayload(RenderableLight* renderable_light)
+		{
+			_renderable_type = RenderableType::Light;
+			_payload = renderable_light;
+		}
+		
+		void SetPayload(RenderableImage* renderable_image)
+		{
+			_renderable_type = RenderableType::Image;
+			_payload = renderable_image;
+		}
+		
+		void ClearPayload()
+		{
+			_renderable_type = RenderableType::None;
+			_payload = nullptr;
+		}
+		
+		RenderableType GetRenderableType() const
+		{
+			return _renderable_type;
+		}
 	};
 }
 
