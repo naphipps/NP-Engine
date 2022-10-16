c9578241823d83b08d88c7d8bc67045a4a339014
Nathan Phipps <naphipps@noreply.com>
2022-10-16 17:08:01 -0500

    minor update to VulkanCamera

--- include/NP-Engine/Graphics/RHI/Vulkan/VulkanCamera.hpp
+++ include/NP-Engine/Graphics/RHI/Vulkan/VulkanCamera.hpp
@@ -13,23 +13,7 @@
 
 namespace np::gfx::rhi
 {
-	class VulkanCamera : public Camera
-	{
-	public:
-		VulkanCamera& operator=(const Camera& other)
-		{
-			Fovy = other.Fovy;
-			AspectRatio = other.AspectRatio;
-			NearPlane = other.NearPlane;
-			FarPlane = other.FarPlane;
-			Eye = other.Eye;
-			Center = other.Center;
-			Up = other.Up;
-			Update();
-
-			return *this;
-		}
-	};
+	using VulkanCamera = Camera;
 } // namespace np::gfx::rhi
 
 #endif /* NP_ENGINE_VULKAN_CAMERA_HPP */
