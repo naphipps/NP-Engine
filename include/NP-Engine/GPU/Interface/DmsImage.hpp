41ccd6cd00689921a09270dd10889ce116ad0e61
Nathan Phipps <naphipps@noreply.com>
2023-05-06 00:19:40 -0500

    updated todo

--- include/NP-Engine/GPU/Interface/DmsImage.hpp
+++ include/NP-Engine/GPU/Interface/DmsImage.hpp
@@ -19,7 +19,7 @@
 #include "DmsLineSegment.hpp"
 #include "FloodFillImage.hpp"
 
-namespace np::alg //TODO: I think we should just use namespace alg
+namespace np::alg //TODO: refactor DMS stuff to DualMarchingSquares
 {
 	class DmsImage
 	{
