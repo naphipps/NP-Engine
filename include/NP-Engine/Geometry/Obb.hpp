a2d8d0f47a9fc34141560168034bf7a0c24f505c
Nathan Phipps <naphipps@noreply.com>
2023-07-08 15:20:39 -0500

    implemented Obb.Contains

--- include/NP-Engine/Geometry/Obb.hpp
+++ include/NP-Engine/Geometry/Obb.hpp
@@ -8,6 +8,7 @@
 #define NP_ENGINE_OBB_HPP
 
 #include "NP-Engine/Primitive/Primitive.hpp"
+#include "NP-Engine/Math/Math.hpp"
 
 #include "Aabb.hpp"
 
@@ -22,12 +23,45 @@ namespace np::geom
 		using Point = typename Aabb<2, T>::Point;
 		::glm::quat orientation{0.f, 0.f, 0.f, 1.f}; //default wxyz {0, 0, 0, 1}
 
+		virtual Point ApplyOrientation(const Point& point) const
+		{
+			Point pt = point - center;
+			::glm::vec4 t = { (flt)pt.x, (flt)pt.y, 0.f, 1.f };
+			t = ::glm::toMat4(orientation) * t;
+			pt = { (T)t.x, (T)t.y };
+			return pt + center;
+		}
+
 		virtual bl Contains(const Point& point) const override
 		{
-			// TODO: implement this
-			// turn point into our orientation
-			Point local_point = point;
-			return Aabb<2, T>::Contains(local_point);
+			return Aabb<2, T>::Contains(ApplyOrientation(point));
+		}
+
+		virtual Aabb<2, T> GetAabb() const
+		{
+			Point a, b, c, d;
+			a = b = c = d = halfLengths;
+
+			b.x *= -1;
+			c.x *= -1;
+			c.y *= -1;
+			d.y *= -1;
+
+			a = ApplyOrientation(a + center);
+			b = ApplyOrientation(b + center);
+			c = ApplyOrientation(c + center);
+			d = ApplyOrientation(d + center);
+
+			Point min, max = {};
+			min.x = ::std::min({ a.x, b.x, c.x, d.x });
+			min.y = ::std::min({ a.y, b.y, c.y, d.y });
+			max.x = ::std::max({ a.x, b.x, c.x, d.x });
+			max.y = ::std::max({ a.y, b.y, c.y, d.y });
+
+			Aabb<2, T> aabb{};
+			aabb.center = center;
+			aabb.halfLengths = max - min;
+			return aabb;
 		}
 	};
 
@@ -37,12 +71,61 @@ namespace np::geom
 		using Point = typename Aabb<3, T>::Point;
 		::glm::quat orientation{0.f, 0.f, 0.f, 1.f}; //default wxyz {0, 0, 0, 1}
 
+		virtual Point ApplyOrientation(const Point& point) const
+		{
+			Point pt = point - center;
+			::glm::vec4 t = { (flt)pt.x, (flt)pt.y, (flt)pt.z, 1.f };
+			t = ::glm::toMat4(orientation) * t;
+			pt = { (T)t.x, (T)t.y, (T)t.z };
+			return pt + center;
+		}
+
 		virtual bl Contains(const Point& point) const override
 		{
-			// TODO: implement this
-			// turn point into our orientation
-			Point local_point = point;
-			return Aabb<3, T>::Contains(local_point);
+			return Aabb<3, T>::Contains(ApplyOrientation(point));
+		}
+
+		virtual Aabb<3, T> GetAabb() const
+		{
+			Point a, b, c, d, e, f, g, h;
+			a = b = c = d = e = f = g = h = halfLengths;
+			
+			b.x *= -1;
+			c.x *= -1;
+			c.y *= -1;
+			d.y *= -1;
+
+			e = a;
+			f = b;
+			g = c;
+			h = d;
+
+			e.z *= -1;
+			f.z *= -1;
+			g.z *= -1;
+			h.z *= -1;
+
+			a = ApplyOrientation(a + center);
+			b = ApplyOrientation(b + center);
+			c = ApplyOrientation(c + center);
+			d = ApplyOrientation(d + center);
+			e = ApplyOrientation(e + center);
+			f = ApplyOrientation(f + center);
+			g = ApplyOrientation(g + center);
+			h = ApplyOrientation(h + center);
+
+			Point min, max = {};
+			min.x = ::std::min({ a.x, b.x, c.x, d.x, e.x, f.x, g.x, h.x });
+			min.y = ::std::min({ a.y, b.y, c.y, d.y, e.y, f.y, g.y, h.y });
+			min.z = ::std::min({ a.z, b.z, c.z, d.z, e.z, f.z, g.z, h.z });
+			max.x = ::std::max({ a.x, b.x, c.x, d.x, e.x, f.x, g.x, h.x });
+			max.y = ::std::max({ a.y, b.y, c.y, d.y, e.y, f.y, g.y, h.y });
+			max.z = ::std::max({ a.z, b.z, c.z, d.z, e.z, f.z, g.z, h.z });
+
+			Aabb<3, T> aabb{};
+			aabb.center = center;
+			aabb.halfLengths = max - min;
+			return aabb;
 		}
 	};
 
