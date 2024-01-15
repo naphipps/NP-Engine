cecd632c379259f6e24a0e280ec371647b7ea132
Nathan Phipps <naphipps@noreply.com>
2024-01-15 11:00:16 -0600

    put full namespace in defines

--- include/NP-Engine/Primitive/PrimitiveValues.hpp
+++ include/NP-Engine/Primitive/PrimitiveValues.hpp
@@ -9,15 +9,15 @@
 
 #include "PrimitiveTypes.hpp"
 
-#define BIT(n) ((siz)1 << (siz)n)
-#define KILOBYTE_SIZE ((siz)1000)
-#define MEGABYTE_SIZE ((siz)1000000)
-#define GIGABYTE_SIZE ((siz)1000000000)
-#define TERABYTE_SIZE ((siz)1000000000000)
+#define BIT(n) ((::np::siz)1 << (::np::siz)n)
+#define KILOBYTE_SIZE ((::np::siz)1000)
+#define MEGABYTE_SIZE ((::np::siz)1000000)
+#define GIGABYTE_SIZE ((::np::siz)1000000000)
+#define TERABYTE_SIZE ((::np::siz)1000000000000)
 
-#define KIBIBYTE_SIZE ((siz)1024)
-#define MEBIBYTE_SIZE ((siz)1048576)
-#define GIBIBYTE_SIZE ((siz)1073741824)
-#define TEBIBYTE_SIZE ((siz)1099511627776)
+#define KIBIBYTE_SIZE ((::np::siz)1024)
+#define MEBIBYTE_SIZE ((::np::siz)1048576)
+#define GIBIBYTE_SIZE ((::np::siz)1073741824)
+#define TEBIBYTE_SIZE ((::np::siz)1099511627776)
 
 #endif /* NP_ENGINE_PRIMITIVE_VALUES_HPP */
